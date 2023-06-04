7431c0176c4a8aa648b4fa4fb10a9adee0fa77d6
Daniel G <code@danielg0.com>
2023-06-04 11:10:26 +0100

    Implement reading from UART
    Whenever we go to read, busy wait until the data available bit in the
    status register of the UART chip (QEMU virt uses 16550 UART) indicates
    there is data to read.
    
    I'm going to move this into it's own source file and implement as a zig
    reader/writer at some point.
    
    Once locks are implemented, there should probably be a global lock(s?)
    protecting access to the UART.

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,24 @@
-const builtin = @import("std").builtin;
+const std = @import("std");
+const builtin = std.builtin;
 
 // -----
 
-const uart = @intToPtr(*volatile u8, 0x10000000);
+// https://www.lammertbies.nl/comm/info/serial-uart
+const LineStatusReg = packed struct {
+    data_available: bool,
+    overrun_error: bool,
+    parity_error: bool,
+    framing_error: bool,
+    break_signal: bool,
+    thr_empty: bool,
+    line_idle: bool,
+    errornous_data: bool,
+};
+const uart_base = 0x10000000;
+const uart_data = @intToPtr(*volatile u8, uart_base);
+const status = @intToPtr(*volatile LineStatusReg, uart_base + 5);
 fn put(char: u8) void {
-    uart.* = char;
+    uart_data.* = char;
 }
 fn print(string: []const u8) void {
     for (string) |c| {
@@ -14,6 +28,26 @@ fn print(string: []const u8) void {
 fn println(string: []const u8) void {
     print(string);
     print("\r\n");
+}
+fn get() u8 {
+    // busy wait for data to be in uart buffer
+    while (!status.data_available) {}
+    return uart_data.*;
+}
+fn getln(buffer: []u8) usize {
+    var n: usize = 0;
+    while (n < buffer.len) {
+        const c = get();
+        put(c);
+        if (c == '\r') {
+            print("\r\n");
+            break;
+        } else {
+            buffer[n] = c;
+            n += 1;
+        }
+    }
+    return n;
 }
 
 // -----
@@ -32,10 +66,31 @@ export fn entry() noreturn {
 // -----
 
 fn main() !void {
+    // test out printing
     const string: []const u8 = "Hello there!";
     println(string);
     println(try error_fn());
 
+    // manually echo a line
+    var c = get();
+    while (c != '\r') {
+        put(c);
+        c = get();
+    }
+    println("");
+
+    // try getting and printing line using helper functions
+    var buff: [100]u8 = undefined;
+    const length = getln(&buff);
+    var msg_buff: [100]u8 = undefined;
+    const msg = try std.fmt.bufPrint(
+        &msg_buff,
+        "You typed {d} characters\n\r{s}",
+        .{ length, buff[0..length] },
+    );
+    println(msg);
+
+    // purposely cause a panic to test out panic handler
     var i: u8 = 0;
     while (true) {
         i += 1;
