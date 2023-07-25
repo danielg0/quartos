64778b0fb7fb90c09a44005c6e90ad83767d19d6
Daniel G <code@danielg0.com>
2023-07-25 14:51:05 +0100

    Hide FDTB printing behind flag
    Printing the whole FDTB tree is quite noisy.

--- src/main.zig
+++ src/main.zig
@@ -5,18 +5,22 @@ const fdtb = @import("boot/fdtb.zig");
 const uart = @import("kernel/uart.zig");
 const process = @import("kernel/process.zig");
 
+const enabled_fdtb = false;
+
 // -----
 
 export fn entry(fdtb_blob: ?[*]const u8) noreturn {
-    if (fdtb_blob) |blob| {
-        fdtb.print(blob, uart.out) catch |e| {
-            uart.out.print("FDTB PARSE ERROR!\r\n{s}\r\n", .{@errorName(e)}) catch unreachable;
+    comptime if (enabled_fdtb) {
+        if (fdtb_blob) |blob| {
+            fdtb.print(blob, uart.out) catch |e| {
+                uart.out.print("FDTB PARSE ERROR!\r\n{s}\r\n", .{@errorName(e)}) catch unreachable;
+                while (true) {}
+            };
+        } else {
+            uart.out.writeAll("FDTB POINTER NULL!\r\n") catch unreachable;
             while (true) {}
-        };
-    } else {
-        uart.out.writeAll("FDTB POINTER NULL!\r\n") catch unreachable;
-        while (true) {}
-    }
+        }
+    };
 
     main() catch |e| {
         uart.out.print("KERNEL PANIC!\r\n{s}\r\n", .{@errorName(e)}) catch unreachable;
