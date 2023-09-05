2bb700d02b2469a6124c3a7e5884da3d10c25919
Daniel G <code@danielg0.com>
2023-09-05 18:06:00 +0100

    Create a trap stack within the Process struct
    When we execute a trap handler, we need a stack for it to run on. I've
    chosen not to use the user programs own stack pointer as it leads to a
    whole bunch of issues (what if it spans different virtual pages or if
    accessing it leads to a page fault?).
    
    Instead, we'll create a per-Process stack that we'll switch to whenever
    we trap. I didn't really have a size in mind, so I've made the whole
    process struct a whole page long in total, with the stack taking up
    however much is left once all the other fields are full.
    
    I've also added a "magic" value we can check that a pointer we have to a
    Process struct actually might point to a Process struct. Note that this
    magic value *won't* necessarily detect a stack overflow, because Zig can
    reorder the fields in a struct.

--- src/kernel/process.zig
+++ src/kernel/process.zig
@@ -3,6 +3,8 @@ const StructList = @import("struct_list.zig").StructList;
 const NAME_LEN = 16;
 const Name = [NAME_LEN]u8;
 
+pub const MAGIC = 0x242;
+
 pub const Process = struct {
     pub const State = enum {
         RUNNING,
@@ -21,7 +23,27 @@ pub const Process = struct {
     // list elements for the all processes and ready/blocked lists
     allelem: StructList.Elem = .{},
     elem: StructList.Elem = .{},
+
+    // magic value we can check for when given a process pointer to check that
+    // it's valid (probably)
+    magic: u32 = MAGIC,
+
+    // the rest of the process struct is used for the interrupt handler stack
+    // TODO: once message passing implemented, come back and consider one
+    // kernel-wide trap stack. If we only ever handle one trap at a time, surely
+    // we only need one trap stack?
+    stack: [STACK_SIZE]u8 = [_]u8{0} ** STACK_SIZE,
+    const STACK_SIZE = 3924;
 };
+
+// we want to keep the size of the process struct equal to a page of memory
+comptime {
+    if (@sizeOf(Process) != std.mem.page_size) {
+        @compileLog("sizeOf(Process):", @sizeOf(Process));
+        @compileLog("sizeOf(Page): ", std.mem.page_size);
+        @compileError("Process struct is not the same size as a page");
+    }
+}
 
 // convert a comptime u8 slice (ie. a string literal), to a process name
 pub fn name(comptime literal: []const u8) Name {
