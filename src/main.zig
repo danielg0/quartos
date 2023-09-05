c8b5cb675a58b8c2b2828f4feabbd49d822e8339
Daniel G <code@danielg0.com>
2023-09-05 18:54:06 +0100

    Add user-level code execution demo to main.zig
    This will mostly need to be removed with something safer at some point
    (currently, there is no memory protection or paging)

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,10 @@ const std = @import("std");
 const builtin = std.builtin;
 
 const fdtb = @import("boot/fdtb.zig");
-const uart = @import("kernel/uart.zig");
 const process = @import("kernel/process.zig");
+const timer = @import("kernel/timer.zig");
+const trap = @import("kernel/trap.zig");
+const uart = @import("kernel/uart.zig");
 
 const enabled_fdtb = false;
 
@@ -41,7 +43,57 @@ fn main() !void {
 
     // initialise ready process and page lists
 
+    // initialise trap handling
+    timer.init();
+    defer timer.deinit();
+
+    trap.init();
+
     // start required processes
+
+    // set up a dummy idle process
+    // TODO: move its source elsewhere?
+    var p: process.Process = .{
+        .id = 0,
+        .name = process.name("idle"),
+        .state = .RUNNING,
+    };
+
+    // set a time for the end of the first slice
+    timer.set(timer.offset(1));
+
+    // attempt to go into user mode
+    _ = asm volatile (
+        \\ csrw mepc, %[pc]
+        // ^ setup initial program counter of user program
+
+        // save pointer to process to mscratch
+        \\ csrw mscratch, %[running]
+
+        // disable memory protection (TODO: remove this)
+        \\ csrwi pmpcfg0, 0x1f
+        \\ li t0, -1
+        \\ csrw pmpaddr0, t0
+
+        // disable virtual memory (TODO: remove this)
+        \\ csrwi satp, 0
+
+        // set which privilege mode to go to
+        \\ li t0, 0x1800
+        \\ csrc mstatus, t0
+
+        // "return from a trap" (ie. jump to mepc as a user)
+        \\ mret
+        :
+        : [pc] "r" (&idle),
+          [running] "r" (&p),
+        : "t0"
+    );
+}
+
+fn idle() noreturn {
+    try uart.out.writeAll("Hi from idle\r\n");
+    while (true) {}
 }
 
 pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace, siz: ?usize) noreturn {
